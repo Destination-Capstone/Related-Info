bb52d2d9008d00be5eae5c9bd5e1b6c1a848ac5b
Collin Newman <collin.newman37@gmail.com>
2021-02-08 18:17:04 -0500

    Changed npm seed script to use bash file. Added csv loading to bash file.

--- database/populateDatabase.sh
+++ database/populateDatabase.sh
@@ -4,7 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 DATABASE="aircmc"
-TABLE1="home"
 USER="collin"
 
 psql -U $USER -d 'postgres' -c 'drop database aircmc;'
@@ -15,6 +14,11 @@ FILEPATH="$DIR/sampleData/"
 SCHEMA="$DIR/schema.sql"
 psql $DATABASE < $SCHEMA
 
-node $DIR/seed.js
+#node $DIR/seed.js
 
-#psql $DATABASE -c "COPY $TABLE1 FROM '$FILEPATH/cities.csv' CSV HEADER;"
+psql $DATABASE -c "COPY city FROM '$FILEPATH/cities.csv' CSV HEADER;"
+psql $DATABASE -c "COPY reservation_type FROM '$FILEPATH/reservationType.csv' CSV HEADER;"
+psql $DATABASE -c "COPY home FROM '$FILEPATH/houses.csv' CSV HEADER;"
+psql $DATABASE -c "COPY house_image FROM '$FILEPATH/houseImages.csv' CSV HEADER;"
+psql $DATABASE -c "COPY activity FROM '$FILEPATH/activities.csv' CSV HEADER;"
+psql $DATABASE -c "COPY activity_image FROM '$FILEPATH/activityImages.csv' CSV HEADER;"
